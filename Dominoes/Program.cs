567b454745d50d444900426ccdf941a4e7c8b674
DiSahian <124094435+DiSahian@users.noreply.github.com>
2023-03-13 19:42:03 +0000

    commit

---
+++ Dominoes/Program.cs
@@ -0,0 +1,26 @@
+﻿class Dominoes
+{
+    //Declaración de parámetros
+    private int d1;
+
+    private int d2;
+
+    //Asignación de parametros a la Clase
+    public Dominoes(int d1, int d2)
+    {
+        this.d1=d1; this.d2=d2;
+    }
+
+    public static int operator +(Dominoes a, Dominoes b)
+    {
+        int suma=a.d1+a.d2+b.d1+b.d2;
+        return suma;
+    }
+    private static void Main(string[] args)
+    {
+        Dominoes d1=new Dominoes(0,1);
+        Dominoes d2=new Dominoes(2,3);
+        int total= d1+d2;
+        Console.WriteLine(total);
+    }
+}
